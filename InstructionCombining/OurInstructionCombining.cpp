d0376b5d47dfb6874f7c1ad3a53a338b678d98d7
jovan <jovanskoric02@gmail.com>
2024-07-02 23:06:38 +0200

    Working on instruction combining

--- InstructionCombining/OurInstructionCombining.cpp
+++ InstructionCombining/OurInstructionCombining.cpp
@@ -4,11 +4,13 @@
 #include "llvm/Support/raw_ostream.h"
 
 #include <map>  // TODO: Unordered map?
+#include <unordered_map>
+#include <llvm/IR/IRBuilder.h>
 using namespace llvm;
 
 namespace {
 std::vector<Instruction *> InstructionsToRemove;
-std::map<Value*, Value*> ValuesMap;
+std::unordered_map<Value*, Value *> ValuesMap;
 // std::map<int, int> addInstrCount;
 
 int addInstrCount = 0;
@@ -28,18 +30,57 @@ struct AddInstrCountPass : public PassInfoMixin<AddInstrCountPass> {
     }
 };
 
+void mapVariables(Function &F) {
+    for (auto &BB : F) {
+        for(auto &I : BB) {
+            if(isa<LoadInst>(&I)) {
+                ValuesMap[&I] = I.getOperand(0);
+            }
+            if(isa<StoreInst>(&I)) {
+                ValuesMap[I.getOperand(0)] = I.getOperand(1);
+            }
+        }
+    }
+}
+
 struct InstructionCombiningPass : public PassInfoMixin<InstructionCombiningPass> {
     PreservedAnalyses run(Module &M, ModuleAnalysisManager &AM) {
-
+        bool changed = false;
         for (auto &F : M) {
+            mapVariables(F);
+            errs() << "Old IR: \n" << F << "\n";
             for (auto &BB : F) {
-                for (auto &I : BB) {
-                    if(LoadInst *LoadInstr  = dyn_cast<LoadInst>(&I)) {
-                        ValuesMap[LoadInstr] = LoadInstr->getOperand(0);
-                    }
+                for(auto &I : BB) {
+                    // if(LoadInst *LoadInstr  = dyn_cast<Loa
+                    errs() << "sss" << "\n";
+                    //     ValuesMap[LoadInstr] = LoadInstr->getOperand(0);
+                    // }
                     if (BinaryOperator *BinaryOp = dyn_cast<BinaryOperator>(&I)) {
-
+                        if(isa<AddOperator>(BinaryOp)) {
+                            if (auto *Const1 = dyn_cast<ConstantInt>(BinaryOp->getOperand(1))) {
+                                //needed to look for other add instructions
+                                for(auto nextIt = std::next(BasicBlock::iterator(BinaryOp)); nextIt != BB.end(); ++nextIt) {
+                                    errs() << "aaa" << "\n";
+                                    if(BinaryOperator *BinaryOp2 = dyn_cast<BinaryOperator>(&*nextIt)) {
+                                        if(isa<AddOperator>(BinaryOp2) && ValuesMap[BinaryOp2->getOperand(0)] == ValuesMap[BinaryOp]) {
+                                            IRBuilder<> Builder(BinaryOp2);
+                                            Instruction *NewAdd = (Instruction*) Builder.CreateAdd(BinaryOp2->getOperand(0),ConstantInt::get(Const1->getType(), 2));
+                                            NewAdd->insertAfter(BinaryOp2);
+                                            BinaryOp2->replaceAllUsesWith(NewAdd);
+                                            InstructionsToRemove.push_back(BinaryOp);
+                                            InstructionsToRemove.push_back(BinaryOp2);                                            
+                                            // changed = true;
+                                            // it = nextIt; // Move iterator to next valid position
+                                            break; // Break out of inner loop to continue with outer loop
+                                        }
+                                    }
+                                }
+                            }
+                        }
                     }
+
+                    if(changed)
+                        break;
                     // errs() << "Inst: " <<  I.getnam << "\n";
                         //   %1 = alloca i32, align 4
                         //   %2 = alloca i32, align 4
@@ -64,9 +105,19 @@ struct InstructionCombiningPass : public PassInfoMixin<InstructionCombiningPass>
                         //   %5 = add nsw i32 %4, 2
                         //   store i32 %5, i32* %3, align 4
                         //   ret i32 0
+                
                     }
                 }
-            }
+        }
+
+        for (Instruction *Instr : InstructionsToRemove) {
+            Instr->eraseFromParent();
+        }
+        
+        for (auto &F : M) {
+            errs() << "new IR: \n" << F << "\n";
+        }
+        errs() << "sadas" << "\n";
         return PreservedAnalyses::all();  // TODO: Change this if we go back to the old pass manager
         }
     };
@@ -80,7 +131,7 @@ llvmGetPassPluginInfo() {
         .RegisterPassBuilderCallbacks = [](PassBuilder &PB) {
             PB.registerPipelineStartEPCallback(
                 [](ModulePassManager &MPM, OptimizationLevel Level) {
-                    MPM.addPass(AddInstrCountPass()),
+                    // MPM.addPass(AddInstrCountPass()),
                     MPM.addPass(InstructionCombiningPass());
                 });
         }
