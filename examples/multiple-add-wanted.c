510252c3e8cb26551a526374dabfaa337f65c82b
jovan <jovanskoric02@gmail.com>
2024-07-02 16:01:30 +0200

    Add example code and wanted example code

---
+++ examples/multiple-add-wanted.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    int y = 1;
+    int z = y + 2;
+
+    return 0;
+}
---
+++ examples/multiple-add.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    int x = 1;
+    int y = x + 1;
+    int z = y + 1;
+
+    return 0;
+}
